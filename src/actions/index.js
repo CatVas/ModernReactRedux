a2892893a23b261eeb9e10b4fb4cc893318f2de4
Catvas <catvaas@googlemail.com>
2016-10-12 10:36:45 +0300

    Redux form validation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 
+export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POSTS = 'FETCH_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -12,5 +13,14 @@ export function fetchPosts() {
   return {
     payload: request,
     type: FETCH_POSTS,
+  };
+}
+
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    payload: request,
+    type: CREATE_POST,
   };
 }
--- src/components/posts.new.js
+++ src/components/posts.new.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router';
 import { reduxForm } from 'redux-form';
+
+import { createPost } from '../actions';
 
 
 class PostsNew extends Component {
@@ -10,31 +14,56 @@ class PostsNew extends Component {
     } = this.props;console.log(this.props);
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a new post</h3>
 
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid && 'has-danger'}`}>
           <label>Title</label>
           <input className="form-control" type="text" { ...title } />
+          <div className="text-help">{title.touched && title.error}</div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid && 'has-danger'}`}>
           <label>Categories</label>
           <input className="form-control" type="text" { ...categories } />
+          <div className="text-help">{categories.touched && categories.error}</div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid && 'has-danger'}`}>
           <label>Content</label>
           <textarea className="form-control" { ...content } ></textarea>
+          <div className="text-help">{content.touched && content.error}</div>
         </div>
 
         <button className="btn btn-primary" type="submit">Submit</button>
+        <Link className="btn btn-danger" to="/">Cancel</Link>
       </form>
     );
   }
 }
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ createPost }, dispatch);
+}
+
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title = `Forgot title? Haven't you forgot your head, have you?`;
+  }
+  if(!values.categories) {
+    errors.categories = 'At least 1 category, please!';
+  }
+  if(!values.content) {
+    errors.content = 'You could say something';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
   fields: ['title', 'categories', 'content'],
   form: 'PostNewForm',
-})(PostsNew);
+  validate,
+}, null, mapDispatchToProps)(PostsNew);
