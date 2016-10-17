550d23b192fe06e7b86e0fb708bbff9bf1f1803d
Catvas <catvaas@googlemail.com>
2016-10-17 09:15:18 +0300

    Redux form is ready

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,30 @@ import axios from 'axios';
 
 
 export const CREATE_POST = 'CREATE_POST';
+export const DELETE_POST = 'DELETE_POST';
 export const FETCH_POST = 'FETCH_POST';
 export const FETCH_POSTS = 'FETCH_POSTS';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=catvas';
+
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    payload: request,
+    type: CREATE_POST,
+  };
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    payload: request,
+    type: DELETE_POST,
+  }
+}
 
 export function fetchPost(id) {
   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
@@ -23,14 +42,5 @@ export function fetchPosts() {
   return {
     payload: request,
     type: FETCH_POSTS,
-  };
-}
-
-export function createPost(props) {
-  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
-
-  return {
-    payload: request,
-    type: CREATE_POST,
   };
 }
--- src/components/post.show.js
+++ src/components/post.show.js
@@ -1,23 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
-import { fetchPost } from '../actions';
+import { deletePost, fetchPost } from '../actions';
 
 
 class PostShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   componentWillMount() {
     const { id } = this.props.params;
 
     this.props.fetchPost(id);
   }
 
+  onPostDelete() {
+    const { id } = this.props.params;
+
+    this.props.deletePost(id).then(() => this.context.router.push('/'));
+  }
+
   render() {
-    return (
+    const { post } = this.props;
+
+    return post ? (
       <div>
-        Show a post {this.props.params.id}
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onPostDelete.bind(this)}
+        >Delete post</button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
+    )
+    : (
+      <div>Loading...</div>
     );
   }
 }
 
-export default connect(null, { fetchPost })(PostShow);
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { deletePost, fetchPost })(PostShow);
